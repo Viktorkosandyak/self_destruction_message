80bf2a151f5986f158b451af6331b4f583605f4d
Viktorkosandyak <kosandyak1996@gmail.com>
2019-01-25 19:12:16 +0200

    first commit

---
+++ db/migrate/20190125170717_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :messages do |t|
+      t.text :body
+      t.boolean :read
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
