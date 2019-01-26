1a28ee8737b5c8e2fa570716d65fa98643ced7bd
Viktorkosandyak <kosandyak1996@gmail.com>
2019-01-26 14:59:50 +0200

    crud messages

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,30 @@
+class MessagesController < ApplicationController
+
+  def index
+      @message = Message.all
+  end
+
+  def show
+      @message = Message.find(params[:id])
+  end
+
+  def new
+      @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to @message
+    else
+      render :new
+    end
+ end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
+end
